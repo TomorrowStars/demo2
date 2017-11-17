7a8fd3a7118c7186704c572c2c490852eea70737
TomorrowStars <chenhongsheng2012@163.com>
2017-11-17 09:59:38 +0800

    20171117_chen_输入属性

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { LifeComponent } from './life/life.component';
+import { OrderComponent } from './order/order.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LifeComponent
+    LifeComponent,
+    OrderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/order/order.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-order',
+  templateUrl: './order.component.html',
+  styleUrls: ['./order.component.css']
+})
+export class OrderComponent implements OnInit {
+
+  @Input()
+  stockCode: string;
+  @Input()
+  amount: number;
+
+  constructor() {
+    setInterval(() => {
+      this.stockCode = "Apple";
+    }, 1000);
+   }
+
+  ngOnInit() {
+  }
+
+}
